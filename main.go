af1b608f11fc2d233328dc8bea2ad8d5d06ba41e
Ali <89438079+AliBa1@users.noreply.github.com>
2025-07-18 16:47:30 -0700

    login command

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/AliBa1/gator/internal/config"
 )
@@ -13,12 +14,42 @@ func main() {
 		return
 	}
 
-	err = c.SetUser("ali")
-	if err != nil {
-		fmt.Println(err.Error())
+	state := config.State{
+		Config: &c,
+	}
+	commands := config.NewCommands()
+
+	if len(os.Args) < 2 {
+		fmt.Printf("Enter a command to use the app")
+		os.Exit(1)
 		return
 	}
 
+	var userCommandName string
+	userCommandName = os.Args[1]
+
+	var args []string
+	if len(os.Args) > 2 {
+		args = os.Args[2:]
+	}
+
+	command := config.Command{
+		Name: userCommandName,
+		Args: args,
+	}
+	err = commands.Run(&state, command)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+		return
+	}
+
+	// err = c.SetUser("ali")
+	// if err != nil {
+	// 	fmt.Println(err.Error())
+	// 	return
+	// }
+	//
 	c, err = config.Read()
 	if err != nil {
 		fmt.Println(err.Error())
